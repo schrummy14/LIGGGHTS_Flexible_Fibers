42396df5ce51b6af1b172a15fbb487a810a74fe2
mschramm <schrummy14@gmail.com>
2020-02-16 14:05:06 -0600

    Updated scripts to work in new directory

--- examples/JKR/jkr_bounce_cor_only/run_case.sh
+++ examples/JKR/jkr_bounce_cor_only/run_case.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -r post post_initial log* restart_*, v_out.txt;
-../../../../src/lmp_auto -echo both < in.*
+../../../src/lmp_auto -echo both < in.*
 
-python post_process.py
+python3 post_process.py
--- examples/JKR/jkr_tan/run_case.sh
+++ examples/JKR/jkr_tan/run_case.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -r post post_initial log* restart_*;
-../../../../src/lmp_auto -echo both < in.*
+../../../src/lmp_auto -echo both < in.*
